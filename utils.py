d627214072f93fe4f8f6a3d401de97162d163a31
imisi99 <isongrichard234@gmail.com>
2024-11-05 11:47:07 +0100

    sklearn logistic model

--- utils.py
+++ utils.py
@@ -6,8 +6,17 @@ def modify_data(file_path):
     print('data before processing...')
     print(df.head())
 
-    df = df.drop(columns=['PassengerId', 'Name'])
+    df = df.drop(columns=['PassengerId', 'Name', 'Ticket'])
 
+    # Fill missing values
+    df['Age'] = df['Age'].fillna(df['Age'].mean())
+    df['Cabin'] = df['Cabin'].fillna('Unknown')
+    df['Embarked'] = df['Embarked'].fillna('Unknown')
+
+    df['Cabin'] = df['Cabin'].str[0]
+    # Convert categorical columns to one-hot encoding
+    df = pd.get_dummies(df, columns=['Embarked', 'Parch', 'SibSp', 'Pclass', 'Cabin', 'Sex'], drop_first=True)
+    df = df.astype(int)
     print('data after processing...')
     print(df.head())
 
